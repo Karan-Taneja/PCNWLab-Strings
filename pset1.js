e07ede20eb4c2b5bbaf1e035dc42f341e4fa8506
Karan Taneja <karantaneja@pursuit.org>
2018-10-15 19:33:25 -0400

    Finished pset1, used loops and helper functions though.

--- pset1.js
+++ pset1.js
@@ -1,7 +1,7 @@
 /*
  *  README!!!
  *  For the problems below, use only string methods / conditionals, etc
- *  it may be tempting to use concepts like looping but do not attempt! 
+ *  it may be tempting to use concepts like looping but do not attempt!
  */
  
  
@@ -16,6 +16,44 @@
     @example - makeEven('john'); // 'john'
 */
 
+    const reverse = input => {
+
+        const reverseArray = []
+
+        for (i = input.length -1; i >= 0; i--){
+            reverseArray.push(input.charAt(i))
+        }
+        const reverseString = reverseArray.join("")
+
+        return reverseString
+    }
+
+    const middleIndex = string => {
+
+        if (string.length % 2 === 0){
+            return string.length / 2
+        }
+
+        else if(string.length % 2 > 0){
+            return ((string.length - 1) / 2)
+        }
+
+    }
+
+    
+
+// End of Help Function    
+
+    const makeEven = name => {
+
+        if (name.length % 2 > 0){
+           return name = name + "#"
+        }
+        return name
+    }
+
+    console.log(makeEven("TK"))
+    console.log(makeEven("Erika"))
 
 /* 2
     @func getLastChar
@@ -25,6 +63,13 @@
     @example - getLastChar('taq'); // 'q'
 */
 
+    const getLastChar = string => {
+        return string.charAt(string.length -1)
+    }
+
+    console.log(getLastChar("TK"))
+    console.log(getLastChar("Erika"))
+
 /* 3
     @func get3rdChar
     @param {string} str
@@ -33,6 +78,12 @@
     @example - get3rdChar('taq'); // 'q'
     @example - get3rdChar('mo'); // 'not enough characters!'
 */
+    const getThirdChar = string => {
+        return string.charAt(2)
+    }
+
+    console.log(getThirdChar("TKhaos"))
+    console.log(getThirdChar("Erika"))
 
 /* 4
     @func get3rdCharFromEnd
@@ -43,6 +94,14 @@
     @example - get3rdCharFromEnd('mo'); // 'not enough characters!'
     @example - get3rdCharFromEnd('taq karim'); // 'r'
 */
+
+    const get3rdCharFromEnd = string => {
+        return string.charAt(string.length - 3)
+    }
+
+
+    console.log(get3rdCharFromEnd("TKhaos"))
+    console.log(get3rdCharFromEnd("Erika"))
 
 /* 5
     @func startsWithVowel
@@ -55,6 +114,28 @@
     @example - startsWithVowel('Andy'); // true
 */
 
+    const startsWithVowel = string => {
+
+    string = string.toLowerCase()
+    let vowelArray = ["a", "e", "i", "o", "u"]
+    let arrayIndex = 0
+    let isVowel = false
+
+        while (arrayIndex < 5 && isVowel === false){
+            if (string.charAt(0) === vowelArray[arrayIndex]){
+                isVowel = true
+            }
+            else {
+                arrayIndex++
+            }
+        }
+        return isVowel
+    }
+
+    console.log(startsWithVowel("TKhaos"))
+    console.log(startsWithVowel("Erika"))
+
+
 /* 6
     @func endsWithVowel
     @param {string} str
@@ -66,6 +147,25 @@
     @example - endsWithVowel('AndI'); // true
 */
 
+const endsWithVowel = string => {
+
+    string = string.toLowerCase()
+    let vowelArray = ["a", "e", "i", "o", "u"]
+    let arrayIndex = 0
+    let isVowel = false
+
+        while (arrayIndex < 5 && isVowel === false){
+            if (string.charAt(string.length -1) === vowelArray[arrayIndex]){
+                isVowel = true
+            }
+            else {
+                arrayIndex++
+            }
+        }
+        return isVowel
+    }
+    console.log(endsWithVowel("TKhaos"))
+    console.log(endsWithVowel("Erika"))
 
 /* 7
     @func hasVowels
@@ -78,6 +178,31 @@
     @example - hasVowels('taq karim'); // true
 */
 
+const containsVowel = string => {
+
+    string = string.toLowerCase()
+    let vowelArray = ["a", "e", "i", "o", "u"]
+    let arrayIndex = 0
+    let charIndex = 0
+    let isVowel = false
+
+    while (charIndex < (string.length) && isVowel === false){
+        while (arrayIndex < 5 && isVowel === false){
+            if (string.charAt(charIndex) === vowelArray[arrayIndex]){
+                isVowel = true
+            }
+            else {
+                arrayIndex++
+            }
+        }
+        arrayIndex = 0
+        charIndex++
+    }
+    return isVowel
+    }
+
+    console.log(containsVowel("TKhaos"))
+    console.log(containsVowel("Erika"))
 
 /* 8
     @func hasUpperCase
@@ -90,6 +215,19 @@
     @example - hasUpperCase('taq karim'); // false
 */
 
+    const hasUpperCase = string => {
+
+        lowerCaseString = string.toLowerCase()
+
+        if (string !== lowerCaseString){
+            return true
+        }
+        return false
+    }
+
+    console.log(hasUpperCase("TKhaos"))
+    console.log(hasUpperCase("Erika"))
+
 /* 9
     @func isPalindromic
     @param {string} str
@@ -100,6 +238,21 @@
     @example - isPalindromic('bats see bees stab'); // false
     @example - hasUpperCase('taq karim'); // false
 */
+
+    const isPalindromic = string => {
+
+        string = string.toLowerCase()
+
+        if (string === reverse(string)){
+            return true
+        }
+
+        return false
+    }
+
+    console.log(isPalindromic("TKhaos"))
+    console.log(isPalindromic("Tacocat"))
+
 
 /* 10
     @func getInitials
@@ -113,6 +266,32 @@
     @example - getInitials('John       Smith'); // "js"
     @example - getInitials('sue RonaN'); // "sr"
 */
+
+    const getInitials = (fullName) => {
+
+        let nameOne = []
+        let nameTwo = []
+        let index = 0
+
+        for (index = 0; fullName.charAt(index) !== " "; index++){
+
+            if (fullName.charAt(index) !== " "){
+                nameOne.push(fullName.charAt(index))
+            }
+
+        }
+
+        for (index; index < fullName.length; index++){
+            if (fullName.charAt(index) !== " "){
+                nameTwo.push(fullName.charAt(index))
+            }
+        }
+
+        return `${nameOne[0]}${nameTwo[0]}`
+
+    }
+    console.log(getInitials("Taneja Karan"))
+    console.log(getInitials("Bruce Wayne"))
 
 /* 11
     @func isPerfectStr
@@ -129,6 +308,42 @@
     @example - isPerfectStr('asdfaeKeccccc'); // true
 */
 
+    const isPerfectStr = string => {
+
+        let findMiddle = middleIndex(string)
+
+        let middleCharacter = string.charAt(findMiddle)
+
+        let vowelArray = ["a", "e", "i", "o", "u"]
+        let isVowel = false
+        let isPalindrome = false
+
+        middleString = `${string.charAt(findMiddle - 1)}${string.charAt(findMiddle)}${string.charAt(findMiddle + 1)}`
+        
+        isPalindrome = isPalindromic(middleString)
+
+        for (i = 0; i < 5; i++){
+            if (middleCharacter === vowelArray[i]){
+                isVowel = true
+            }
+        }
+
+        if (string.length > 3 && isVowel === true && isPalindrome === true) {
+            return true
+        }
+
+        return false
+    }
+
+console.log(isPerfectStr("ab")) //false
+console.log(isPerfectStr("hog")) //false
+console.log(isPerfectStr("abtotab")) //true
+console.log(isPerfectStr('abctotabc')); // true
+
+// 123 ,  456,  789 //char num
+// "abc","tot","abc"
+// 012,   345,  678//index num
+
 /* 12
     @func strMasher
     @param {string} str1
@@ -140,3 +355,19 @@
     @example - capitalLast('hello', 'wrold'); // "helwroldlo"
 */
 
+const strMasher = (stringOne, stringTwo) => {
+
+    stringOneArray = []
+
+    for (i = 0; i < stringOne.length; i++){
+        stringOneArray.push(stringOne.charAt(i))
+    }
+
+    middle = middleIndex(stringOneArray) + 1
+
+    stringOneArray.splice(middle, 0, stringTwo)
+
+    return stringOneArray.join("")
+}
+
+console.log(strMasher("Hello", "Wrold"))
